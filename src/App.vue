893a2dac207c1629d57455564ffd6a201d61895d
jurvantoledo <jurvantoledo@live.nl>
2021-01-29 17:37:58 +0100

    made dates work

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,11 @@
           <div class="location">
             {{ weather.name }}, {{ weather.sys.country }}
           </div>
-          <div class="date">Friday 29, January 2021</div>
+          <div class="date">{{ dateBuilder() }}</div>
 
           <div class="weather-box">
-            <div class="temp">9°c</div>
-            <div class="weather">Rain</div>
+            <div class="temp">{{ Math.round(weather.main.temp) }}</div>
+            <div class="weather">{{ weather.weather[0].main }}</div>
           </div>
         </div>
       </div>
@@ -54,6 +54,39 @@ export default {
     },
     setResults(results) {
       this.weather = results;
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
     },
   },
 };
